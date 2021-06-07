dddb1d07cfd0c14a0950e9dde72240e6e19bee47
Kilian Ciuffolo <kciuffolo@roblox.com>
2021-06-07 12:44:24 +0200

    chore: remove remaining yarn references

--- src/proto/codegen.sh
+++ src/proto/codegen.sh
@@ -3,18 +3,19 @@
 set -e
 
 proto_dir="src/proto"
+npm_bin=$(npm bin)
 
 # generate javascript code
-yarn grpc_tools_node_protoc \
+"$npm_bin/grpc_tools_node_protoc" \
   --js_out="import_style=commonjs,binary:${proto_dir}" \
   --grpc_out="${proto_dir}" \
-  --plugin="protoc-gen-grpc=$(npm bin)/grpc_tools_node_protoc_plugin" \
+  --plugin="protoc-gen-grpc=$npm_bin/grpc_tools_node_protoc_plugin" \
   -I "${proto_dir}" \
   "${proto_dir}"/*.proto
 
 # generate typescript ambient declaration (d.ts)
-yarn grpc_tools_node_protoc \
-  --plugin="protoc-gen-ts=$(npm bin)/protoc-gen-ts" \
+"$npm_bin/grpc_tools_node_protoc" \
+  --plugin="protoc-gen-ts=$npm_bin/protoc-gen-ts" \
   --ts_out="${proto_dir}" \
   -I "${proto_dir}" \
   "${proto_dir}"/*.proto

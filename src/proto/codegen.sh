da927a871a1a8440c180fc6695f367da6bb6090e
Kilian Ciuffolo <385716+kilianc@users.noreply.github.com>
2023-10-08 08:23:07 -0700

    chore: use npx after npm bin deprecation (#259)

--- src/proto/codegen.sh
+++ src/proto/codegen.sh
@@ -3,14 +3,13 @@
 set -e
 
 proto_dir="src/proto"
-npm_bin="node_modules/.bin"
 
 rm -rf -- \
   "${proto_dir:?}"/*.ts \
   "${proto_dir:?}"/*.js \
   "${proto_dir:?}"/*/
 
-"$npm_bin/proto-loader-gen-types" \
+npx proto-loader-gen-types \
   --longs=String \
   --enums=String \
   --defaults \
